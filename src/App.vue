50f6fe628dc77576a96f250998937dfba9fd114d
XY <xiayingspace@gmail.com>
2020-10-22 20:56:33 +0800

    fast push

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
     <Header/>
+
     <router-view/>
-<!--    <Footer/>-->
+
+    <Footer/>
   </div>
 </template>
 
 <script>
   import Header from "./components/Header";
-  // import Footer from "./components/Footer";
+  import Footer from "./components/Footer";
   export default {
     name: 'app',
     components: {
       Header,
-      // Footer
+      Footer
     }
   }
 </script>
