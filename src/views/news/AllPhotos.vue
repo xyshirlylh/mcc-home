5183179b2c3076bc918d4665772fb7bf90596bd1
YulongZheng <36038078+YulongZheng@users.noreply.github.com>
2021-06-30 18:10:04 +0800

    新闻汇总页面修改完成

--- src/views/news/AllPhotos.vue
+++ src/views/news/AllPhotos.vue
@@ -37,9 +37,7 @@ export default {
     //this.currentUrl = window.location.href;
     //console.log(this.currentUrl);
     this.newsID = urlParams.get("id");
-    this.content = require("../../assets/news/" +
-      this.newsID +
-      "/content.json");
+    this.content = this.$i18n.t("message.news." + this.newsID);
     this.allPhotos = new Array(this.content.photocount);
   },
 
@@ -75,69 +73,79 @@ export default {
 </script>
 
 <style lang="scss">
-.all-photos {
-  padding: (118px) 0 (167px) 0;
-  .button-back {
-    position: fixed;
-    left: 100vw/1920 * 42;
-    display: flex;
-    flex-direction: row;
-    padding: 0 0 0 0;
-    //margin-left: 100vw/1920 * 42;
-    height: 100vw/1920 * 34;
-    width: 100vw/1920 * 98.45;
-    border-radius: 2.5rem;
-    text-align: center;
-    cursor: pointer;
+@mixin styling($unit-size) {
+  .all-photos {
+    .button-back {
+      position: fixed;
+      left: 27 * $unit-size;
+      top: 72 * $unit-size;
+      height: 0 * $unit-size;
+      display: flex;
+      flex-direction: row;
+      padding: 0 0 0 0;
+      //margin-left: 100vw/1920 * 42;
+      height: 21 * $unit-size;
+      width: 100vw/1920 * 98.45;
+      border-radius: 4px;
+      text-align: center;
+      cursor: pointer;
 
-    p {
-      line-height: 100vw/1920 * 34;
+      p {
+        line-height: 21 * $unit-size;
+      }
+
+      .arrow-back {
+        height: 100vw/1920 * 13.71;
+        width: 100vw/1920 * 9;
+        margin-left: 20%;
+        margin-right: 10%;
+
+        img {
+          margin-top: -3 * $unit-size;
+          width: 10.66 * $unit-size;
+          height: 10.66 * $unit-size;
+        }
+      }
+
+      &:hover {
+        background-color: rgba(229, 229, 229, 1);
+      }
+      font: Source Sans Pro;
+      font-size: 11 * $unit-size;
+      line-height: 21 * $unit-size;
     }
+    padding: (118px) 0 (167px) 0;
 
-    .arrow-back {
-      height: 100vw/1920 * 13.71;
-      width: 100vw/1920 * 9;
-      margin-left: 20%;
-      margin-right: 10%;
+    .gallery {
+      width: 70vw;
+
+      margin-left: auto;
+      margin-right: auto;
 
       img {
-        margin-top: 0.5rem;
-        width: 100%;
-        height: 100%;
+        width: 500px;
+        height: 300px;
+        object-fit: cover;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        //width: 500px;
+        background-color: white;
+      }
+
+      //img {
+      //  width: 100%;
+      // }
+
+      .item1 {
+        //height: 46.2rem;
       }
     }
-
-    &:hover {
-      background-color: rgba(229, 229, 229, 1);
-    }
-    font: normal normal normal 2rem/2.8rem Source Sans Pro;
   }
-  .gallery {
-    width: 70vw;
-
-    margin-left: auto;
-    margin-right: auto;
-
-    img {
-      width: 500px;
-      height: 300px;
-      object-fit: cover;
-
-      display: flex;
-      justify-content: center;
-      align-items: center;
-
-      //width: 500px;
-      background-color: white;
-    }
-
-    //img {
-    //  width: 100%;
-    // }
-
-    .item1 {
-      //height: 46.2rem;
-    }
-  }
+}
+@media (max-width: 100vw) {
+  @include styling($unit-size: 100vw/1366);
 }
 </style>
